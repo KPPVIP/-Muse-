55fadf3dc7ef0f0ca603f09465684a545d08ec3b
Guudz <Kilian1702@outlook.com>
2023-01-28 22:39:04 +0100

    Initial commit

---
+++ [script]/bArtHeist/Sql.sql
@@ -0,0 +1,7 @@
+INSERT INTO `items` (`name`, `label`, `weight`) VALUES
+	('painting_1', 'Peinture de niveau 1', 1),
+	('painting_2', 'Peinture de niveau 2', 1),
+	('painting_3', 'Peinture de niveau 3', 1),
+	('golden_egg', 'Oeuf d or', 1),
+	('laser_drill', 'Perceuse laser', 1),
+	('winch', 'Treuil', 1);
---
+++ [script]/bArtHeist/locales/en.lua
@@ -0,0 +1,44 @@
+Locales['en'] = {
+    ['unlock_doors'] = "Presse ~y~[E]~w~ déverrouiller les portes",
+    ['lock_doors'] = "Presse ~y~[E]~w~ verrouiller les portes",
+    ['hack_systems'] = "Presse ~y~[E]~w~ pirater le système",
+    ['hack_vault'] = "Presse ~y~[E]~w~ pirater le non-sens",
+    ['setup_winch'] = "Presse ~y~[E]~w~ placer le treuil",
+    ['rapeal_window'] = "Presse ~y~[E]~w~ faire appel par la fenêtre",
+    ['cut_window'] = "Presse ~y~[E]~w~ couper la fenêtre",
+    ['cut_glass'] = "Presse ~y~[E]~w~ couper le verre",
+    ['fix_window'] = "Presse ~y~[E]~w~ réparer la fenêtre",
+    ['attach_rope'] = "Presse ~y~[E]~w~ attacher la corde",
+    ['detach_rope'] = "Presse ~y~[G]~w~ lâcher la corde",
+    ['steal_painting'] = "Presse ~y~[E]~w~ voler la peinture",
+    ['steal_egg'] = "Presse ~y~[E]~w~ voler l'oeuf",
+    ['contact_dealer'] = "Presse ~y~[E]~w~ appeler le marchand du marché noir",
+    ['sell_blackmarket'] = "Presse ~y~[E]~w~ vendez vos objets de valeur",
+    
+    ['robbery_cancelled'] = "Le cambriolage a été annulé",
+    ['not_enough'] = "Il n'y a pas assez de policiers en ligne en ce moment",
+    ['time_bought'] = "Vous avez du temps. Vous avez %s secondes jusqu'à ce que l'alarme se déclenche",
+    ['already_robbed'] = "La galerie d'art a déjà été récemment volée",
+    ['being_robbed'] = "La galerie d'art est volée !",
+    ['dont_have'] = "Vous avez besoin d'un %s  pour faire cela",
+    ['blip_name'] = "Musée",
+    ['currency'] = "$%s",
+    ['blackmarket'] = "Marché de l'art",
+        
+    ['time_left']    = "Vous avez  %s  secondes pour éteindre l'alarme",
+    ['skill_failed']    = "Vous avez échoué au test de compétence",
+
+    ['prog_cut_hole'] = "Trou",
+    ['prog_set_winch'] = "Placer le treuil",
+    ['prog_contact_dealer'] = "Contacter le vendeur du marché noir",
+    ['prog_steal_painting'] = "Voler la peinture",
+    ['prog_steal_egg'] = "Voler l'oeuf",
+    ['prog_hacking']    = "Pirater le système",
+
+    ['subject_heading']    = "Lieu de ramassage",
+    ['dealer_loc']    = "Activité suspecte",
+    ['text_message']    = "Je te retrouve sur le site de livraison, je ne peux pas rester longtemps, tu as 30 secondes",
+}
+
+-- E = ~INPUT_CONTEXT~
+-- G = ~INPUT_DETONATE~
